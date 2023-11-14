c7d0ab9034ec64e2c29216070eeb64afc2a9474a
Sergii Pryima <spryima@gmail.com>
2023-11-14 23:56:51 +0200

    product list and detail view added

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product-list"),
+    path("<int:pk>", ProductDetailView.as_view(), name="product-detail"),
 ]
 
 app_name = "products"
--- products/views.py
+++ products/views.py
@@ -1,9 +1,22 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list.html'
-    context_object_name = 'products'
+    template_name = "products/product_list.html"
+    context_object_name = "products"
+    paginate_by = 5
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+
+
+
+# class AddToCartListView(ListView):
+#     def post(self, request, product_id):
+
+
