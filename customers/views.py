aded9692c081e6d92131fc6e4d407e33930c0c1b
Sergii Pryima <spryima@gmail.com>
2023-11-15 17:53:56 +0200

    transfer cart from unknown user to customer combining carts after login

--- customers/views.py
+++ customers/views.py
@@ -1,14 +1,43 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic import DetailView
 
+from cart.models import ShoppingCart, CartItem
 from customers.models import Customer
+from products.models import Product
 
 
 class CustomerLoginView(LoginView):
+    form_class = AuthenticationForm
     redirect_authenticated_user = True
-    next_page = reverse_lazy('home')
+
+    def form_valid(self, form):
+        customer = form.get_user()
+        login(self.request, customer)
+
+        self.transfer_cart_to_user(customer)
+
+        return super().form_valid(form)
+
+    def transfer_cart_to_user(self, customer):
+        cart_session = self.request.session.get("cart", {})
+        if cart_session:
+            for product_id, quantity in cart_session.items():
+                product = Product.objects.get(id=int(product_id))
+                cart, _ = ShoppingCart.objects.get_or_create(customer=customer)
+                cart_item, created = CartItem.objects.get_or_create(
+                    cart=cart, product=product
+                )
+                if created:
+                    cart_item.quantity = quantity
+                else:
+                    cart_item.quantity += quantity
+                cart_item.save()
+
+            self.request.session["cart"] = {}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,4 +46,3 @@ class CustomerLoginView(LoginView):
 
 class CustomerDetailView(LoginRequiredMixin, DetailView):
     model = Customer
-
