34d71528796ede19553c020f443819f168434c69
Sergii Pryima <spryima@gmail.com>
2023-11-15 19:26:30 +0200

    category filtration & pagitation added

--- products/views.py
+++ products/views.py
@@ -4,13 +4,26 @@ from django.views import View
 from django.views.generic import ListView, DetailView
 
 from cart.models import ShoppingCart
-from .models import Product
+from .models import Product, Category
 
 
 class ProductListView(ListView):
     model = Product
     context_object_name = "products"
     paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_ids = self.request.GET.getlist("category_id")
+        if category_ids:
+            queryset = queryset.filter(category__id__in=category_ids)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        context["selected_category_ids"] = self.request.GET.getlist("category_id")
+        return context
 
 
 class ProductDetailView(DetailView):
