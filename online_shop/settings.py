edbd40a11473fc9302966a2ceeb8527372a0eed5
Sergii Pryima <spryima@gmail.com>
2023-11-17 18:13:13 +0200

    debug change to false

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", "django-insecure-r*-9a66p0ze2f$2wt!p&9)lz-ly(o+05hd$plx+)!8tvoqc536")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
