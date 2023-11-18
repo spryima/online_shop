bc550a965da711beadf1fb53dc6603fa4cee0c20
Sergii Pryima <spryima@gmail.com>
2023-11-18 16:55:16 +0200

    index.html space update

--- customers/forms.py
+++ customers/forms.py
@@ -3,11 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from customers.models import Customer
 
 
-class CustomerCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = Customer
-        fields = UserCreationForm.Meta.fields + (
-            "first_name",
-            "last_name",
-            "address",
-        )
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
